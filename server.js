d76c33ecf516fe2275a7f25a647168ee7df3d2a1
Chris <chomsey1@gmail.com>
2019-02-17 23:23:38 -0500

    added route for production environment to serve build/index.html

--- server.js
+++ server.js
@@ -15,11 +15,6 @@ app.use(cors());
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/graphQLBooksDB';
 const PORT = process.env.PORT || 4000;
 
-// Serve up static assets on Heroku
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
-
 // connect to local Mongo database
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 mongoose.connection.once('open', ()=> console.log('connected to database!'))
@@ -29,9 +24,12 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
+// Serve up static assets on Heroku
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+}
 
 app.listen(PORT, ()=> console.log(`Server is listening on port ${PORT}`));
