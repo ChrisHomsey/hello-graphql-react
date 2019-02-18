96c05b47a3bf24e23454b576bf786d1aababd944
Chris <chomsey1@gmail.com>
2019-02-17 21:33:23 -0500

    client changes for heroku deploy

---
+++ client/build/precache-manifest.851d35d720850aeddae5f59fd8eac7e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1ab9f07d2d1a6f028a3b",
+    "url": "/static/js/main.1ab9f07d.chunk.js"
+  },
+  {
+    "revision": "e7ceffb7a3dfda5e40d9",
+    "url": "/static/js/2.e7ceffb7.chunk.js"
+  },
+  {
+    "revision": "1ab9f07d2d1a6f028a3b",
+    "url": "/static/css/main.29f20ca1.chunk.css"
+  },
+  {
+    "revision": "4b19945d534fdc82a431073b0ba35a8b",
+    "url": "/index.html"
+  }
+];
